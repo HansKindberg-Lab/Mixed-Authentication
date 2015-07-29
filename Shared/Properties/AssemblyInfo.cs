65100a85a956d953c150be730a60ff1d7bf51fa2
Hans Kindberg <hans.kindberg@gmail.com>
2015-07-29 11:17:51 +0200

    Code cleanup.

--- Shared/Properties/AssemblyInfo.cs
+++ Shared/Properties/AssemblyInfo.cs
@@ -14,7 +14,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © LIT 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
 [assembly: CLSCompliant(true)]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
